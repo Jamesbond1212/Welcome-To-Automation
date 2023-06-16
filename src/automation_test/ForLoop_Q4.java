4cb753d2138ac5c1946c463c1e0fb18d008907ee
Jamesbond1212 <casinoroyaljamesbond7@gmail.com>
2023-06-16 20:57:16 +0100

    Java Test Submitted

---
+++ src/automation_test/ForLoop_Q4.java
@@ -0,0 +1,17 @@
+package automation_test;
+
+public class ForLoop_Q4 {
+    public void printMyName(String name, int howManyTime){
+        for(int i = 0; i<howManyTime;i++){
+            System.out.println(name);
+        }
+    }
+
+    public static void main(String[] args) {
+        ForLoop_Q4 obj = new ForLoop_Q4();
+        obj.printMyName("James", 10);
+        obj.printMyName("Bond", 15);
+    }
+
+
+}
